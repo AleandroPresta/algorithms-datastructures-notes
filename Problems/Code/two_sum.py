9420cd80bfb223846a7f9d4c20a3e3449ee42137
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-07-01 11:20:25 +0200

    chore: Refactor two_sum function for improved readability and maintainability

--- Problems/Code/two_sum.py
+++ Problems/Code/two_sum.py
@@ -15,11 +15,21 @@ def two_sum(nums, target):
     >>> target = 9
     >>> two_sum(nums, target)
     [0, 1]  # Indices of numbers 2 and 7 which add up to 9
+    
+    Complexity:
+    - Time: O(n)
+      The function iterates over the list 'nums' once (O(n)), and each dictionary lookup
+      or insertion operation in 'num_map' averages O(1). Thus, the overall time complexity
+      is O(n).
+
+    - Space: O(n)
+      The space complexity is O(n) due to the usage of 'num_map', which can store up to 
+      'n' elements in the worst case scenario where no pair is found.
     """
-    num_map = {}  # Dictionary to store seen numbers and their indices
+    num_map = {}  # Dictionary (HashMap) to store seen numbers and their indices
     for i, num in enumerate(nums):
         complement = target - num  # Calculate the complement needed to reach 'target'
-        if complement in num_map:
+        if complement in num_map: # Search the complement in num_map
             # If complement exists in num_map, return the indices of the current number and complement
             return [num_map[complement], i]
         num_map[num] = i  # Store the current number and its index in num_map
