29fda9c3f620090b4a473b6bf8ea0a216c4a1079
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-01 11:50:46 +0200

    Merge corrected

--- leetcode/arrays/merge.py
+++ leetcode/arrays/merge.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from icecream import ic
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
@@ -14,17 +14,28 @@ class Solution:
         i = 0
         j = 0
         k = 0
-        while (i < m or j < n):
-            if (nums1_copy[i] <= nums2[j] and i < m):
+        while i < m and j < n:
+            if (nums1_copy[i] < nums2[j]):
                 nums1[k] = nums1_copy[i]
                 i += 1
-                k += 1
             else:
                 nums1[k] = nums2[j]
-                j += 1
-                k += 1
+                j += 1             
+            k += 1
+            
+        # If there are remaining elements in nums1_copy
+        while i < m:
+            nums1[k] = nums1_copy[i]
+            i += 1
+            k += 1
         
-    
+        # If there are remaining elements in nums2
+        while j < n:
+            nums1[k] = nums2[j]
+            j += 1
+            k += 1
+
+
 class Test:
     def test1(self):
         nums1 = [1,2,3,0,0,0]
@@ -52,14 +63,26 @@ class Test:
         Solution().merge(nums1, m, nums2, n)
         expected_result = [1]
         assert nums1 == expected_result
-        
+    
+    def test4(self):
+        nums1 = [2,0]
+        m = 1
+        nums2 = [1]
+        n = 1
+        Solution().merge(nums1, m, nums2, n)
+        expected_result = [1, 2]
+        assert nums1 == expected_result
 
 def main() -> None:
     test = Test()
     test.test1()
+    print('Test 1 passed')
     test.test2()
+    print('Test 2 passed')
     test.test3()
-    print('All test passed')
+    print('Test 3 passed')
+    test.test4()
+    print('Test 4 passed')
     
 if __name__ == '__main__':
     main()
